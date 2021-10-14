d05cb9269c80f59bd80a833039649170684e2409
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2021-10-14 13:52:26 +0800

    fix bugs

--- goconf.go
+++ goconf.go
@@ -1,6 +1,7 @@
 package goconf
 
 import (
+	"errors"
 	"fmt"
 	"go/ast"
 	"go/parser"
@@ -15,6 +16,14 @@ type Config struct {
 	Value   string
 	Comment string
 }
+
+type CanSetString interface {
+	SetString(string) error
+}
+
+var (
+	ErrNotStruct = errors.New("target must be a struct or a pointer to a struct")
+)
 
 func ToConfigs(c interface{}) (configs []Config) {
 	rt := reflect.TypeOf(c)
@@ -38,6 +47,16 @@ const (
 `
 	rt := reflect.TypeOf(c)
 	rv := reflect.ValueOf(c)
+	if !rv.IsValid() {
+		return nil, ErrNotStruct
+	}
+	if rt.Kind() == reflect.Ptr {
+		rt = rt.Elem()
+		rv = rv.Elem()
+	}
+	if rt.Kind() != reflect.Struct {
+		return nil, ErrNotStruct
+	}
 	for i := 0; i < rt.NumField(); i++ {
 		if i > 0 {
 			output += "\n"
@@ -87,11 +106,20 @@ func Unmarshal(input []byte, v interface{}) error {
 				}
 				for _, name := range vs.Names {
 					field := rv.Elem().FieldByName(name.Name)
+					if !field.IsValid() {
+						continue
+					}
+					if field.Kind() == reflect.Ptr && field.IsNil() {
+						field.Set(reflect.New(field.Type().Elem()))
+					}
 					if field.Kind() == reflect.String {
 						field.SetString(value)
-					} else if i, ok := field.Addr().Interface().(interface{ SetString(string) error }); ok {
-						err := i.SetString(value)
-						if err != nil {
+					} else if i, ok := field.Interface().(CanSetString); ok {
+						if err := i.SetString(value); err != nil {
+							return err
+						}
+					} else if i, ok := field.Addr().Interface().(CanSetString); ok {
+						if err := i.SetString(value); err != nil {
 							return err
 						}
 					}
--- goconf_test.go
+++ goconf_test.go
@@ -3,7 +3,49 @@ package goconf
 import (
 	"encoding/json"
 	"fmt"
+	"testing"
 )
+
+type bar struct{ Code string }
+
+func (b *bar) SetString(input string) error { b.Code = input; return nil }
+func (b bar) String() string                { return b.Code }
+
+func Test(t *testing.T) {
+	if _, err := Marshal(nil); err != ErrNotStruct {
+		t.Error("Marshal(nil) should return ErrNotStruct")
+	}
+	if _, err := Marshal(1); err != ErrNotStruct {
+		t.Error("Marshal(1) should return ErrNotStruct")
+	}
+	if _, err := Marshal("a"); err != ErrNotStruct {
+		t.Error("Marshal(a) should return ErrNotStruct")
+	}
+	a := &struct {
+		Name string
+		Bar  *bar
+		Old  string
+	}{"foo", &bar{"bar"}, "old"}
+	c, err := Marshal(a)
+	if err != nil {
+		panic(err)
+	}
+	var b struct {
+		Bar  *bar
+		Name string
+		New  string
+	}
+	err = Unmarshal(c, &b)
+	if err != nil {
+		panic(err)
+	}
+	if a.Name != b.Name {
+		t.Error("name should be equal")
+	}
+	if a.Bar.Code != b.Bar.Code {
+		t.Error("code should be equal")
+	}
+}
 
 func ExampleMarshal() {
 	a := struct {
